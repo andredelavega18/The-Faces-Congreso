d3a285a543f7a432712cd6519a79a14d0188e62e
Andre Cahuana <andrex12852@gmail.com>
2026-02-16 18:31:19 -0500

    fix: use checkout layout with beforeInteractive pixel for reliable loading

---
+++ src/app/checkout/layout.tsx
@@ -0,0 +1,38 @@
+import Script from 'next/script';
+
+export default function CheckoutLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <Script id="facebook-pixel" strategy="beforeInteractive">
+                {`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;
+                    s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '1546145490022800');
+                    fbq('track', 'PageView');
+                    fbq('track', 'InitiateCheckout');
+                `}
+            </Script>
+            <noscript>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src="https://www.facebook.com/tr?id=1546145490022800&ev=PageView&noscript=1"
+                    alt=""
+                />
+            </noscript>
+            {children}
+        </>
+    );
+}
