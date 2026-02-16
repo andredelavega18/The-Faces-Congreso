306fb1316dafbb24d3bdea32eba40eaf09917300
Andre Cahuana <andrex12852@gmail.com>
2026-02-16 18:49:45 -0500

    fix: use dangerouslySetInnerHTML for pixel to guarantee SSR rendering

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,5 +1,3 @@
-import Script from 'next/script';
-
 export default function CheckoutLayout({
     children,
 }: {
@@ -7,37 +5,40 @@ export default function CheckoutLayout({
 }) {
     return (
         <>
-            <Script id="facebook-pixel" strategy="beforeInteractive">
-                {`
-                    !function(f,b,e,v,n,t,s)
-                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                    if(!f._fbq)f._fbq=n;
-                    n.push=n;
-                    n.loaded=!0;
-                    n.version='2.0';
-                    n.queue=[];
-                    t=b.createElement(e);
-                    t.async=!0;
-                    t.src=v;
-                    s=b.getElementsByTagName(e)[0];
-                    s.parentNode.insertBefore(t,s)}(window, document,'script',
-                    'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '1546145490022800');
-                    fbq('trackSingle', '1546145490022800', 'PageView');
-                    fbq('trackSingle', '1546145490022800', 'InitiateCheckout');
-                `}
-            </Script>
-            <noscript>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                    height="1"
-                    width="1"
-                    style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=1546145490022800&ev=PageView&noscript=1"
-                    alt=""
+            <head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;
+                            n.push=n;
+                            n.loaded=!0;
+                            n.version='2.0';
+                            n.queue=[];
+                            t=b.createElement(e);
+                            t.async=!0;
+                            t.src=v;
+                            s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '1546145490022800');
+                            fbq('trackSingle', '1546145490022800', 'PageView');
+                            fbq('trackSingle', '1546145490022800', 'InitiateCheckout');
+                        `,
+                    }}
                 />
-            </noscript>
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=1546145490022800&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
+            </head>
             {children}
         </>
     );
